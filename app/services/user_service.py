b7641ee40ee95784d462757ca386ed4496e3682a
Rakesh N <nraki522@gmail.com>
2026-05-18 12:13:53 +0530

    created the user update service

--- app/services/user_service.py
+++ app/services/user_service.py
@@ -1,4 +1,5 @@
 from sqlalchemy.orm import Session
+from fastapi import HTTPException,status
 from app.models.user import User
 from app.core.utils import hash_password
 
@@ -23,6 +24,25 @@ def get_user_service(db:Session,id):
     user = db.query(User).filter(User.id ==id ).first()
     return user
 
+
+def update_user_service(db: Session, user_id: int, user_update):
+    user = db.query(User).filter(User.id == user_id).first()
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"User with id {user_id} not found"
+        )
+
+    update_data = user_update.model_dump(exclude_unset=True)
+    if "password" in update_data and update_data["password"]:
+        update_data["password"] = hash_password(update_data["password"])
+
+    for key, value in update_data.items():
+        setattr(user, key, value)
+
+    db.commit()
+    db.refresh(user)
+    return user
 
 def delete_user_service(db:Session,id):
     user = db.query(User).filter(User.id == id).first()
